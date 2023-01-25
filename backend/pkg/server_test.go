13361e3f6a06caf72438c0049131b23e9cb1f29c
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-01-25 11:08:16 -0300

    add addQuestions test

--- backend/pkg/server_test.go
+++ backend/pkg/server_test.go
@@ -1,9 +1,11 @@
 package pkg_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"fairwinds.com/anonymous-questions-app/pkg"
@@ -28,7 +30,23 @@ func TestListQuestions(t *testing.T) {
 }
 
 func TestAddQuestions(t *testing.T) {
-	// TODO
+	r := setupServer(t)
+	defer cleanUp()
+
+	w := httptest.NewRecorder()
+
+	payload := strings.NewReader(`{"Text": "Is this a good question?"}`)
+
+	req, _ := http.NewRequest("POST", "/api/v0/questions", payload)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+	var q pkg.Question
+	err := json.Unmarshal(w.Body.Bytes(), &q)
+	assert.NoError(t, err)
+	assert.NotZero(t, q.ID)
+	assert.Zero(t, q.UpVotes)
+	assert.Equal(t, "Is this a good question?", q.Text)
 }
 
 func setupServer(t *testing.T) *gin.Engine {
