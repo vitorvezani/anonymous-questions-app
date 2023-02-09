b0cc46eec204a464f15f09ff38f28b0c2e51cd9a
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-02-09 11:18:38 -0300

    add gin integration

--- backend/go.mod
+++ backend/go.mod
@@ -3,11 +3,15 @@ module fairwinds.com/anonymous-questions-app
 go 1.16
 
 require (
-	github.com/gin-contrib/cors v1.4.0 // indirect
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.2
 	github.com/glebarez/sqlite v1.6.0
-	github.com/rs/cors v1.8.3 // indirect
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.1
-	gorm.io/gorm v1.24.3
+	github.com/uptrace/opentelemetry-go-extra/otelgorm v0.1.21 // indirect
+	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.39.0
+	go.opentelemetry.io/otel v1.13.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.13.0
+	go.opentelemetry.io/otel/sdk v1.13.0
+	gorm.io/gorm v1.24.5
 )
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"fairwinds.com/anonymous-questions-app/pkg"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/glebarez/sqlite"
 	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 	"gorm.io/gorm"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
 func main() {
@@ -17,6 +26,7 @@ func main() {
 
 	db.AutoMigrate(&pkg.Question{})
 
+	
 	h, err := pkg.NewHandler(db)
 	if err != nil {
 		logrus.Fatal("could not create handler", err)
@@ -25,10 +35,36 @@ func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 
+	tp, err := initTracer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	r.Use(otelgin.Middleware("anonymous-questions-backend"))
+
 	s, err := pkg.NewServer(r, h)
 	if err != nil {
 		logrus.Fatal("could not create server", err)
 	}
 
 	logrus.Fatal(s.Start())
+}
+
+func initTracer() (*sdktrace.TracerProvider, error) {
+	exporter, err := stdout.New(stdout.WithPrettyPrint())
+	if err != nil {
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, nil
 }
