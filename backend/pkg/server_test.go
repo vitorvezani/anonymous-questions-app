08fbd45212009e01a5cb0c49b29bfd9ee11d6151
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-02-13 18:17:48 -0300

    remove tests implementation

--- backend/pkg/server_test.go
+++ backend/pkg/server_test.go
@@ -1,7 +1,6 @@
 package pkg_test
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -18,75 +17,23 @@ import (
 var dbFile = "sqlite_test.db"
 
 func TestListQuestions(t *testing.T) {
-	r := setupServer(t)
-	defer cleanUp()
-
-	req, _ := http.NewRequest("GET", "/api/v0/questions", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "[]", w.Body.String())
+	// TODO
 }
 
 func TestAddQuestionsError(t *testing.T) {
-	r := setupServer(t)
-	defer cleanUp()
-
-	payload := strings.NewReader(`{"Text": "Is this a good question"}`)
-
-	req, _ := http.NewRequest("POST", "/api/v0/questions", payload)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-	assert.Equal(t, `{"error":"Key: 'Question.Text' Error:Field validation for 'Text' failed on the 'questionValidator' tag"}`, w.Body.String())
+	// TODO
 }
 
 func TestAddQuestions(t *testing.T) {
-	r := setupServer(t)
-	defer cleanUp()
-
-	payload := strings.NewReader(`{"Text": "Is this a good question?"}`)
-
-	req, _ := http.NewRequest("POST", "/api/v0/questions", payload)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusCreated, w.Code)
-	var q pkg.Question
-	err := json.Unmarshal(w.Body.Bytes(), &q)
-	assert.NoError(t, err)
-	assert.NotZero(t, q.ID)
-	assert.Zero(t, q.UpVotes)
-	assert.Equal(t, "Is this a good question?", q.Text)
+	// TODO
 }
 
 func TestDeleteQuestions(t *testing.T) {
-	r := setupServer(t)
-	defer cleanUp()
-
-	addQuestion(t, r)
-
-	req, _ := http.NewRequest("DELETE", "/api/v0/questions", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusNoContent, w.Code)
+	// TODO
 }
 
 func TestUpVoteQuestion(t *testing.T) {
-	r := setupServer(t)
-	defer cleanUp()
-
-	addQuestion(t, r)
-
-	req, _ := http.NewRequest("POST", "/api/v0/questions/1/up-vote", nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, `{"up_votes":1}`, w.Body.String())
+	// TODO
 }
 
 func addQuestion(t *testing.T, r *gin.Engine) {
--- backend/pkg/util_test.go
+++ backend/pkg/util_test.go
@@ -6,27 +6,5 @@ import (
 
 // go test -run ^TestValidateQuestion$ fairwinds.com/anonymous-questions-app/pkg -v
 func TestValidateQuestion(t *testing.T) {
-
-	// Given XYZ
-	// if I do XYZ
-	// I expect XYZ
-
-	var tests = []struct {
-		i    string
-		want bool
-	}{
-		{"is this a question", false},
-		{"?is this a question", false},
-		{"is this a question¿", false},
-		{"is this a question?", true},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.i, func(t *testing.T) {
-			ans := validateQuestion(tt.i)
-			if ans != tt.want {
-				t.Errorf("got %v, want %v", ans, tt.want)
-			}
-		})
-	}
+	// TODO
 }
